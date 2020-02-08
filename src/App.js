64da2115789b9d7a9bd8728bbe4edeb5490f1dd8
Sunil Kumar <45172271+edocbuhtig@users.noreply.github.com>
2020-02-09 00:18:00 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Tableauholder from './components/tableau';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Tableauholder />
+      </div>
+    );
+  }
+}
+
+export default App;
